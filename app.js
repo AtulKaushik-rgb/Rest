83e95e8ac3896c7270aaabce7b1da7c8e28fc238
Atul Kaushik <kaushikatul033@gmail.com>
2020-12-06 10:33:08 +0530

    Completed entire rest api functionality

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const express = require("express");
 const mongoose = require('mongoose');
 const connectionString = require('dotenv/config');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.json());
 
 //Middlewares
 // app.use('/posts',()=>{
 //     console.log("this is a middleware running")
 // })
 
+//use cors
+app.use(cors());
+
+
 //Connect to database
 mongoose.connect(
     process.env.DB_CONNECTION,
+    //'mongodb://Atul:kaushik36@mongoclusterforatul.snpzo.mongodb.net/MongoClusterForAtul',
     { useNewUrlParser: true },
     ()=>{
-    console.log("connected to db")
+    console.log("connected to db with query",process.env.DB_CONNECTION)
 })
 
 //Import routes
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,72 @@
 const express = require("express");
-
 const router = express.Router();
+const Post = require("../models/Post.js");
 
-router.get('/',(req,res)=>{
-    res.send("Hello we are on posts")
+//Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
+router.get("/specials", (req, res) => {
+  res.send("special post");
+});
 
-router.get('/specials',(req,res)=>{
-    res.send("special post")
+//Add any posts
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//get specific post by id
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//delete a specific post
+
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//update a specific post
+
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      {
+        $set: {
+          title: req.body.title,
+        }
+      }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
